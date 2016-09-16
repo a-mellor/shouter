ecf1f7aa14a6d25497151a6f47fcea7da51c9428
alex <alexander.mellor@gmail.com>
2016-09-16 15:17:24 +0100

    Vaildates presence of shout

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -4,8 +4,11 @@ class ShoutsController < ApplicationController
   end
   def create
     shout = current_user.shouts.build(shout_paramaters)
-    shout.save
-    redirect_to dashboard_path
+    if shout.save
+      redirect_to dashboard_path
+    else flash.alert = "Could not shout"
+      redirect_to dashboard_path
+    end
   end
 
   private
